e7bbf25e57414dc5489651dda033020bd7fcc605
Muhammad Saif <muhammadsaifarain786@gmail.com>
2024-02-24 21:02:29 +0500

    deployed

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Todo-List-App---Typescript-version/' // Change this to match your repository name
+});
